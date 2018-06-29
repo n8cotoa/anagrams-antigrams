8aa8468d6ca986e1dc211a74c9de0ab7d93386c0
Nate Cottle <cottle.nate@gmail.com>
2018-06-29 08:37:49 -0700

    add first positive pass.

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -4,6 +4,6 @@ require('anagrams_antigrams')
 describe('#anagrams_antigrams') do
   it('program should return true is words are anagrams') do
     words = Word.new('ruby', 'bury')
-    expect(words.anagram?()).to(eq(false))
+    expect(words.anagram?()).to(eq(true))
   end
 end
